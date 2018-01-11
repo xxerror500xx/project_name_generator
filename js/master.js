7f83e76ea068ff205f65ef8cc75fd9fa4225c3b7
xxerror500xx <cliff.minks@gmail.com>
2018-01-11 17:10:00 -0600

    add bootstrap card classes to css for a cleaner presentation

--- js/master.js
+++ js/master.js
@@ -1,12 +1,6 @@
 // Project names start with an adjective then a Noun traditionaly
 // nouns, pronouns, verbs, adjectives, adverbs, conjunctions, prepositions, and interjections
 
-const commonNouns = ["people", "history", "way", "art", "world",
-  "information", "map", "two", "family", "government", "health", "system",
-  "computer", "meat", "year", "thanks", "music", "person", "reading",
-  "method", "data", "food", "understanding", "theory", "law", "bird",
-  "literature"
-];
 const commonAdjectives = ["able", "bad", "best", "better", "big", "black",
   "certain", "clear", "different", "early", "easy", "economic", "false", "federal",
   "free", "full", "good", "great", "hard", "high", "human", "important",
@@ -15,11 +9,30 @@ const commonAdjectives = ["able", "bad", "best", "better", "big", "black",
   "possible", "public", "real", "recent", "right", "small", "social",
   "special", "strong", "sure", "true", "white", "whole", "young"
 ];
-
+const commonNouns = ["people", "history", "way", "art", "world",
+  "information", "map", "two", "family", "government", "health", "system",
+  "computer", "meat", "year", "thanks", "music", "person", "reading",
+  "method", "data", "food", "understanding", "theory", "law", "bird",
+  "literature"
+];
 var getRandomWord = function(wordType) {
-
   return wordType[Math.floor(Math.random() * wordType.length)];
-}
+};
+var capitalizeFirstLetter = function(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+var getListStatsAdj = function() {
+  document.getElementById('numAdj').innerHTML = commonAdjectives.length;
+};
+var getListStatsNoun = function() {
+  document.getElementById('numNouns').innerHTML = commonNouns.length;
+};
 var generateProjectName = function() {
-  document.getElementById('currentName').innerHTML = getRandomWord(commonAdjectives) + " " + getRandomWord(commonNouns)
+  document.getElementById('currentName').innerHTML = capitalizeFirstLetter(getRandomWord(commonAdjectives)) + " " + getRandomWord(commonNouns);
+};
+var onLoad = function() {
+  getListStatsAdj();
+  getListStatsNoun();
 }
+onLoad();
